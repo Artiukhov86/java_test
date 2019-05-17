f1574c8e580f4c577727d5add04cd2ea52dd9d67
Artiukhov86 <50213469+Artiukhov86@users.noreply.github.com>
2019-05-17 22:21:51 +0300

    Создал объекты

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,12 @@ public class MyFirstProgram {
 
 public static void main(String[] args) {
 
-System.out.println("Hello, world! ");
+    System.out.println("Hello, world! ");
 
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area() );
+
+    Rectangle r = new Rectangle(8, 10);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area() );
 }
 }
